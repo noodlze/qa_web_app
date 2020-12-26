d0346acb33d93ec60d10c669b6b0ea56d5271ab5
KatePham <thuy.pham@notabasement.com>
2020-12-26 21:21:26 +0700

    get answer apis

--- api/resources/answer.py
+++ api/resources/answer.py
@@ -1,20 +1,27 @@
-from flask_restful import Resource,abort
+from flask_restful import Resource, abort
 from models import Answer, Question, with_row_locks
 from utils.db_session import provide_db_session
-from flask import request
+from flask import request, jsonify
 from datetime import datetime
+
+
 class OneAnswerResource(Resource):
 
-    def get(self,qn_id,ans_id): # get an answer
-        Answer.get(ans_id)
-        #TODO:
+    def get(self, qn_id, ans_id):  # get an answer
+        answer = Answer.get(ans_id)
+        if not answer:
+            abort(400)
+        return {"msg": {"created_at": answer.created_at,
+                        "content": answer.content,
+                        "likes": answer.likes,
+                        "dislikes": answer.dislikes}}, 200
 
     @provide_db_session
     def put(self, qn_id, ans_id, db=None):  # update an answer
         json_data = request.get_json(force=True)
-        new_content = json_data.get('content',None)
-        new_likes = json_data.get('likes',None)
-        new_dislikes = json_data.get('dislikes',None)
+        new_content = json_data.get('content', None)
+        new_likes = json_data.get('likes', None)
+        new_dislikes = json_data.get('dislikes', None)
 
         existing_answer = Answer.get(ans_id)
 
@@ -32,7 +39,7 @@ class OneAnswerResource(Resource):
 
         return {"msg": "Updated answer={}".format(ans_id)}, 200
 
-    def delete(self,qn_id, ans_id): # delete an answer
+    def delete(self, qn_id, ans_id):  # delete an answer
         Answer.delete(ans_id)
 
         return {"msg": "Delete answer={}".format(ans_id)}, 200
@@ -40,12 +47,25 @@ class OneAnswerResource(Resource):
 
 class OneQuestionAnswersResource(Resource):
     @provide_db_session
-    def get(self,qn_id,db=None): # get all answers of a question
-        ans_list = with_row_locks(db.session.query(Answer).filter(Answer.qn_id == qn_id).order_by(Answer.created_at)).all()
-        #TODO:
+    def get(self, qn_id, db=None):  # get all answers of a question
+        ans_list = with_row_locks(db.session.query(Answer).filter(
+            Answer.qn_id == qn_id).order_by(Answer.created_at)).all()
+        if not ans_list:
+            abort(400)
+        results = []
+        for r in ans_list:
+            result = {}
+            result["created_at"] = r.created_at
+            result["content"] = r.content
+            result["likes"] = r.likes
+            result["dislikes"] = r.dislikes
+
+            results.append(result)
+
+        return jsonify(results), 200
 
     @provide_db_session
-    def post(self,qn_id,db=None): # add new answer to a question
+    def post(self, qn_id, db=None):  # add new answer to a question
         json_data = request.get_json(force=True)
 
         content = json_data.get('content', None)
@@ -56,11 +76,11 @@ class OneQuestionAnswersResource(Resource):
 
         db.session.add(new_answer)
 
-        return {"msg": "Added answer={} to qn={}".format(content,qn_id)}, 200
+        return {"msg": "Added answer={} to qn={}".format(content, qn_id)}, 200
 
     @provide_db_session
-    def delete(self,qn_id,db=None): # delete all answers of a question
+    def delete(self, qn_id, db=None):  # delete all answers of a question
         with_row_locks(db.session.query(Answer).filter(
             Answer.qn_id == qn_id)).delete()
 
-        return {"msg": "Deleted all answers of qn={}".format( qn_id)}, 200
+        return {"msg": "Deleted all answers of qn={}".format(qn_id)}, 200
